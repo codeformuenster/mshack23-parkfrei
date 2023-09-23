3062f8a3eb2230e1bd96aa2f6053dbb1bd2e6409
thunfischtoast <thunfischtoast@posteo.de>
2023-09-23 12:04:57 +0200

    working detection

--- docs/js/scripts_impressions.js
+++ docs/js/scripts_impressions.js
@@ -4,6 +4,124 @@ const slider = document.getElementById("slider");
 const imageSelect = document.getElementById("imageSelect");
 const fileInput = document.getElementById('fileInput');
 const previewImage = document.getElementById('previewImage');
+const detectSubmit = document.getElementById('detect_submit');
+
+import {
+    ObjectDetector,
+    FilesetResolver,
+    // Detection,
+    // ObjectDetectionResult
+  } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2";
+  
+let objectDetector;
+let runningMode = "IMAGE";
+
+// Initialize the object detector
+const initializeObjectDetector = async () => {
+    const vision = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.2/wasm"
+    );
+    objectDetector = await ObjectDetector.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath: `https://storage.googleapis.com/mediapipe-models/object_detector/efficientdet_lite0/float16/1/efficientdet_lite0.tflite`,
+        delegate: "GPU"
+      },
+      scoreThreshold: 0.3,
+      runningMode: runningMode
+    });
+    // demosSection.classList.remove("invisible");
+  };
+  initializeObjectDetector();
+
+
+detectSubmit.addEventListener("click", handleClick);
+
+
+/**
+ * Detect objects in still images on click
+ */
+async function handleClick(event) {
+    const highlighters = event.target.parentNode.getElementsByClassName(
+      "highlighter"
+    );
+    while (highlighters[0]) {
+      highlighters[0].parentNode.removeChild(highlighters[0]);
+    }
+  
+    const infos = event.target.parentNode.getElementsByClassName("info");
+    while (infos[0]) {
+      infos[0].parentNode.removeChild(infos[0]);
+    }
+  
+    if (!objectDetector) {
+      alert("Object Detector is still loading. Please try again.");
+      return;
+    }
+  
+    // if video mode is initialized, set runningMode to image
+    if (runningMode === "VIDEO") {
+      runningMode = "IMAGE";
+      await objectDetector.setOptions({ runningMode: "IMAGE" });
+    }
+  
+    const ratio = event.target.height / event.target.naturalHeight;
+  
+    // objectDetector.detect returns a promise which, when resolved, is an array of Detection objects
+    const detections = objectDetector.detect(previewImage);
+    console.log(detections);
+    displayImageDetections(detections, previewImage);
+  }
+
+  function displayImageDetections(
+    result,
+    resultElement
+  ) {
+    const ratio = resultElement.height / resultElement.naturalHeight;
+    console.log(ratio);
+  
+    for (let detection of result.detections) {
+      // Description text
+      const p = document.createElement("p");
+      p.setAttribute("class", "info");
+      p.innerText =
+        detection.categories[0].categoryName +
+        " - with " +
+        Math.round(parseFloat(detection.categories[0].score) * 100) +
+        "% confidence.";
+      // Positioned at the top left of the bounding box.
+      // Height is whatever the text takes up.
+      // Width subtracts text padding in CSS so fits perfectly.
+      p.style =
+        "left: " +
+        detection.boundingBox.originX * ratio +
+        "px;" +
+        "top: " +
+        detection.boundingBox.originY * ratio +
+        "px; " +
+        "width: " +
+        (detection.boundingBox.width * ratio - 10) +
+        "px;";
+      const highlighter = document.createElement("div");
+      highlighter.setAttribute("class", "highlighter");
+      highlighter.style =
+        "left: " +
+        detection.boundingBox.originX * ratio +
+        "px;" +
+        "top: " +
+        detection.boundingBox.originY * ratio +
+        "px;" +
+        "width: " +
+        detection.boundingBox.width * ratio +
+        "px;" +
+        "height: " +
+        detection.boundingBox.height * ratio +
+        "px;";
+  
+      resultElement.parentNode.appendChild(highlighter);
+      resultElement.parentNode.appendChild(p);
+    }
+  }
+
 
 function updateImageVisibility() {
     const sliderValue = slider.value;
